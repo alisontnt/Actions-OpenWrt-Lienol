15dc391041113c3589222d9eac4e4b8b680e14ea
AlisonTNT <violencezheng@gmail.com>
2021-08-29 12:37:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,5 +9,5 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
-sed -i 's/4.14/4.9/g' target/linux/x86/Makefile
+sed -i 's/192.168.1.1/10.7.30.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/4.14/4.9/g' target/linux/x86/Makefile
